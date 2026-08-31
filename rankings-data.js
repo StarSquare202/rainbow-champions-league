d4b25dfec8a630103ad516107dfea5a22a56d70c
StarSquare202 <62736962+StarSquare202@users.noreply.github.com>
2026-08-31 13:57:37 +0900

    Update rankings-data.js

--- rankings-data.js
+++ rankings-data.js
@@ -4,75 +4,93 @@ window.RCL_DATA = {
   "sports": [
     {
       "sport": "팔씨름(오른팔)",
+      "sportEn": "ARM WRESTLING · RIGHT",
       "rankings": [
         {
           "rank": "1st",
-          "name": "장민선"
+          "name": "장민선",
+          "nameEn": "Jang Min-seon"
         },
         {
           "rank": "2rd",
-          "name": "윤소원"
+          "name": "윤소원",
+          "nameEn": "Yoon So-won"
         },
         {
           "rank": "3rd",
-          "name": "이슬기"
+          "name": "이슬기",
+          "nameEn": "Lee Seul-gi"
         },
         {
           "rank": "4th",
-          "name": "강류리"
+          "name": "강류리",
+          "nameEn": "Kang Ryu-ri"
         },
         {
           "rank": "5th",
-          "name": "송진주"
+          "name": "송진주",
+          "nameEn": "Song Jin-joo"
         },
         {
           "rank": "6th",
-          "name": "백물개"
+          "name": "백물개",
+          "nameEn": "Baek Mul-gae"
         },
         {
           "rank": "7th",
-          "name": "전채원"
+          "name": "전채원",
+          "nameEn": "Jeon Chae-won"
         },
         {
           "rank": "8th",
-          "name": "이지나"
+          "name": "이지나",
+          "nameEn": "Lee Ji-na"
         }
       ]
     },
     {
       "sport": "팔씨름(왼팔)",
+      "sportEn": "ARM WRESTLING · LEFT",
       "rankings": [
         {
           "rank": "1st",
-          "name": "백물개"
+          "name": "백물개",
+          "nameEn": "Baek Mul-gae"
         },
         {
           "rank": "2rd",
-          "name": "이슬기"
+          "name": "이슬기",
+          "nameEn": "Lee Seul-gi"
         },
         {
           "rank": "3rd",
-          "name": "강류리"
+          "name": "강류리",
+          "nameEn": "Kang Ryu-ri"
         },
         {
           "rank": "4th",
-          "name": "장민선"
+          "name": "장민선",
+          "nameEn": "Jang Min-seon"
         },
         {
           "rank": "5th",
-          "name": "윤소원"
+          "name": "윤소원",
+          "nameEn": "Yoon So-won"
         },
         {
           "rank": "6th",
-          "name": "송진주"
+          "name": "송진주",
+          "nameEn": "Song Jin-joo"
         },
         {
           "rank": "7th",
-          "name": "전채원"
+          "name": "전채원",
+          "nameEn": "Jeon Chae-won"
         },
         {
           "rank": "8th",
-          "name": "이지나"
+          "name": "이지나",
+          "nameEn": "Lee Ji-na"
         }
       ]
     }
