0eeee884672696a8638a92b523aa1dc9af20a215
StarSquare202 <62736962+StarSquare202@users.noreply.github.com>
2026-08-31 13:32:57 +0900

    Add rankings-data.js

---
+++ rankings-data.js
@@ -0,0 +1,81 @@
+// rank.xlsx에서 자동 생성된 파일입니다. 직접 수정하지 마세요.
+window.RCL_DATA = {
+  "updatedAt": "2026-08-31T04:02:13.006Z",
+  "sports": [
+    {
+      "sport": "팔씨름(오른팔)",
+      "rankings": [
+        {
+          "rank": "1st",
+          "name": "장민선"
+        },
+        {
+          "rank": "2rd",
+          "name": "윤소원"
+        },
+        {
+          "rank": "3rd",
+          "name": "이슬기"
+        },
+        {
+          "rank": "4th",
+          "name": "강류리"
+        },
+        {
+          "rank": "5th",
+          "name": "송진주"
+        },
+        {
+          "rank": "6th",
+          "name": "백물개"
+        },
+        {
+          "rank": "7th",
+          "name": "전채원"
+        },
+        {
+          "rank": "8th",
+          "name": "이지나"
+        }
+      ]
+    },
+    {
+      "sport": "팔씨름(왼팔)",
+      "rankings": [
+        {
+          "rank": "1st",
+          "name": "백물개"
+        },
+        {
+          "rank": "2rd",
+          "name": "이슬기"
+        },
+        {
+          "rank": "3rd",
+          "name": "강류리"
+        },
+        {
+          "rank": "4th",
+          "name": "장민선"
+        },
+        {
+          "rank": "5th",
+          "name": "윤소원"
+        },
+        {
+          "rank": "6th",
+          "name": "송진주"
+        },
+        {
+          "rank": "7th",
+          "name": "전채원"
+        },
+        {
+          "rank": "8th",
+          "name": "이지나"
+        }
+      ]
+    }
+  ]
+};
+
